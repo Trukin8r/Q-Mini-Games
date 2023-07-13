e629f405446f274cb8a41e9cd021a738923816d3
Trukin8r <134828118+Trukin8r@users.noreply.github.com>
2023-07-13 14:21:49 -0500

    Set style and linking with temporary content

---
+++ script.js
@@ -0,0 +1,11 @@
+function cardAlt(e) {
+    document.getElementById(e).style.backgroundColor = "LightGreen";
+    document.getElementById(e).style.border = "3px solid LightGreen";
+}
+function cardReg(e) {
+    document.getElementById(e).style.backgroundColor = "LightBlue";
+    document.getElementById(e).style.border = "3px solid MidnightBlue";
+}
+function cardClick(e) {
+    
+}
