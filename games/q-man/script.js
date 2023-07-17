22e4208bbe68d7b64e506d5a05e5c893ae7dea65
Trukin8r <134828118+Trukin8r@users.noreply.github.com>
2023-07-17 01:11:41 -0500

    Added logic and format for letter blanks

--- games/q-man/script.js
+++ games/q-man/script.js
@@ -1,19 +1,20 @@
 let mute = false;
 let target;
 let keyP = "";
-let wordFile = "./easywords.csv"
-let regLet = /[a-zA-Z]/
+let wordFile = "./easywords.csv";
+let regLet = /[a-zA-Z]/;
+let secret;
 
 document.addEventListener('keydown', keyPress)
 // document.addEventListener('keyup', keyPress(keyP))
 function keyPress(k, regex) {
     // let targetK = k.currentTarget;
     let char = k.key;
-    console.log(char.length)
+    console.log(char.length);
     if(char.length == 1) {
         if(regLet.test(char)) {
-            char = char.toUpperCase()
-            letterClick(char)
+            char = char.toUpperCase();
+            letterClick(char);
         }
     }
 }
@@ -38,24 +39,24 @@ function cancelButton() {
 }
 function restartButton() {
     if(confirm('Do you really want to lose your progress and restart Q-Man with a new word?')){
-        restart()
+        restart();
     }
 }
 function settingsButton() {
 
 }
 function snowflakeButton() {
-
+    getWord()
 }
 
 function letterClick(l) {
-    const input = document.getElementById('letter' + l)
+    const input = document.getElementById('letter' + l);
     if(input.className == 'letterCard'){
         if(mute == false) {
-            new Audio('./sounds/interface-button.mp3').play()
+            new Audio('./sounds/interface-button.mp3').play();
         }
-        input.className = 'usedLetterCard'
-        letterSolver(l)
+        input.className = 'usedLetterCard';
+        letterSolver(l);
 
         console.log(l);
     }
@@ -66,3 +67,27 @@ function letterClick(l) {
 function restart(){
     // need some work here to restart the game after a game completes or when the restart button is pressed
 }
+
+function getWord() {
+    secret = mWords[Math.floor((Math.random() * mWords.length) + 1)];
+    console.log(secret);
+    setWord(secret);
+}
+
+function setWord(w) {
+    const gameArea = document.getElementById('gameArea');
+    let letterBlank = document.createElement('div');
+    while (gameArea.firstChild) {
+        gameArea.removeChild(gameArea.firstChild)
+    }
+    console.log(w.length)
+    for(i=1; i < (w.length + 1); ++i){
+        letterBlank.className = 'letterBlank';
+        letterBlank.id = 'letter' + i;
+        letterBlank.innerHTML = ' ';
+        gameArea.appendChild(letterBlank.cloneNode(true));
+        console.log(letterBlank.id)
+        console.log(gameArea.childNodes)
+    }
+        
+    }
