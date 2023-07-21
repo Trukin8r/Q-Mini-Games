0403ddeb0fec9825b57c9e2d7086a85adbf4f517
Trukin8r <134828118+Trukin8r@users.noreply.github.com>
2023-07-20 23:54:39 -0500

    Add banner, logo and initial js code

--- games/q-ship/game.js
+++ games/q-ship/game.js
@@ -1,6 +1,14 @@
 const canvas = document.querySelector('canvas');
-
+const ctx = canvas.getContext('2d');
 canvas.width = 1020;
 canvas.height = 600;
 
+ctx.fillStyle = 'black'
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+function showTP(toolTip){
+    toolTip.style.display = 'block';
+}
+function hideTP(toolTip){
+    toolTip.style.display = '';
+}
