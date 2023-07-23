603d4133a2d9ea3c697bb22deef231010bcbfb03
Trukin8r <134828118+Trukin8r@users.noreply.github.com>
2023-07-22 20:31:33 -0500

    Add space junk, mines and images

--- games/q-ship/game.js
+++ games/q-ship/game.js
@@ -1,6 +1,10 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 const torpedos = [];
+const mines = [];
+const junk = [];
+const qortal = [];
+const images = [];
 let sSpeed = 3;
 let rSpeed = .04;
 let friction = .97;
@@ -19,7 +23,23 @@ const keys = {
         press: false
     }
 }
+function loadImages() {
+    for ( i=0; i < 10; i++) {
+        images[i] = new Image();
+    }
+    images[0].src = './images/qortal_overlay.png';
+    images[1].src = './images/TechF.png';
+    images[2].src = './images/TechG.png';
+    images[3].src = './images/TechM.png';
+    images[4].src = './images/MoneyF.png';
+    images[5].src = './images/MoneyE.png';
+    images[6].src = './images/MoneyD.png';
+    images[7].src = './images/GovC.png';
+    images[8].src = './images/GovC.png';
+    images[9].src = './images/GovC.png';
+  }
 
+  loadImages()
 window.addEventListener('keydown', (e) => {
     switch (e.code) {
         case 'KeyW':
@@ -57,6 +77,100 @@ canvas.height = 600;
 
 ctx.fillStyle = 'black'
 ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+const objectInterval = window.setInterval(() => {
+    const index = Math.floor(Math.random() * 4)
+    const type = Math.floor(Math.random() * 4)
+    let x, y
+    let vx, vy
+    let radius = 70 * Math.random() + 25
+    let img;
+    switch (index) {
+      case 0: // left side of the screen
+        x = 0 - radius;
+        y = Math.random() * canvas.height;
+        vx = 1;
+        if (Math.random() > .5) {
+            vy = Math.random() * 2;
+        } else {
+            vy = Math.random() * -2;
+        }
+        break;
+      case 1: // bottom side of the screen
+        x = Math.random() * canvas.width;
+        y = canvas.height + radius;
+        if (Math.random() > .5) {
+            vx = Math.random() * 2;
+        } else {
+            vx = Math.random() * -2;
+        }
+        vy = -1;
+        break;
+      case 2: // right side of the screen
+        x = canvas.width + radius
+        y = Math.random() * canvas.height
+        vx = -1;
+        if (Math.random() > .5) {
+            vy = Math.random() * 2;
+        } else {
+            vy = Math.random() * -2;
+        }
+        break;
+      case 3: // top side of the screen
+        x = Math.random() * canvas.width
+        y = 0 - radius
+        if (Math.random() > .5) {
+            vx = Math.random() * 2;
+        } else {
+            vx = Math.random() * -2;
+        }
+        vy = 1
+        break
+    }
+    console.log(type)
+    if (type < 3){
+        switch(type) {
+            case 0:
+                img = (Math.floor(Math.random() * 3)) + 7;
+                break;
+            case 1:
+                img = (Math.floor(Math.random() * 3)) + 4;
+                break;
+            case 2:
+                img = (Math.floor(Math.random() * 3)) + 1;
+                break;
+        }
+        junk.push(
+        new Junk({
+            posit: {
+            x: x,
+            y: y,
+            },
+            velo: {
+            x: vx,
+            y: vy,
+            },
+            radius,
+            type, img,
+        })
+        )
+    } else if (Math.random() < .5) {
+        console.log('Qortal')
+        qortal.push(
+            new Qortal({
+                posit: {
+                x: x,
+                y: y,
+                },
+                velo: {
+                x: vx,
+                y: vy,
+                },
+                radius,
+                })
+            )
+    }
+  }, 3000)
 
 function launch() {
     if (torpedos.length < tCount){
@@ -172,6 +286,98 @@ class Torpedos {
         
     }
 }
+class Junk {
+    constructor({ posit, velo, radius, type, img }) {
+        this.posit = posit;
+        this.velo = velo;
+        this.radius = radius;
+        this.type = type;
+        this.img = img;
+        
+    }
+    spawn() {
+        
+        ctx.drawImage(
+            images[this.img],
+            this.posit.x,
+            this.posit.y,
+            this.radius,
+            this.radius
+        )
+        
+    }
+    move() {
+        this.spawn();
+        this.posit.x += this.velo.x;
+        this.posit.y += this.velo.y;
+        if (this.type == 0) {
+            if (this.radius < 125){
+                this.radius += .25
+            } else {
+                this.radius += .1
+            }
+        }
+        
+        
+    }
+}
+class Qortal {
+    constructor({ posit, velo, radius, }) {
+        this.posit = posit;
+        this.velo = velo;
+        this.radius = 15;
+        
+    }
+    spawn() {
+        ctx.drawImage(
+                // document.getElementById('qImg'),
+                images[0],
+                this.posit.x,
+                this.posit.y,
+                25,
+                25
+            )
+    }
+    move() {
+        this.spawn();
+        this.posit.x += this.velo.x;
+        this.posit.y += this.velo.y;
+        
+    }
+}
+class Mine {
+    constructor({ posit, velo,}) {
+        this.posit = posit;
+        this.velo = velo;
+        this.radius = 5;
+        // if (Math.random() > .5) {
+        //     this.xvelo = Math.random() * 1;
+        // } else {
+        //     this.xvelo = Math.random() * -1;
+        // }
+        // if (Math.random() > .5) {
+        //     this.yvelo = Math.random() * 1;
+        // } else {
+        //     this.yvelo = Math.random() * -1;
+        // }
+        // this.radius = 5;
+        
+    }
+    spawn() {
+        ctx.beginPath();
+        ctx.arc(this.posit.x, this.posit.y, this.radius, 0, Math.PI * 2, false);
+        ctx.fillStyle = 'red';
+        ctx.fill();
+        ctx.closePath();
+    }
+    move() {
+        this.spawn();
+        this.posit.x += this.velo.x;
+        this.posit.y += this.velo.y;
+        
+    }
+}
+
 const ship = new Ship({
     posit: { x: canvas.width / 2, y: canvas.height / 2 },
     velo: { x: 0, y: 0 },
@@ -182,12 +388,9 @@ function play() {
     ctx.fillStyle = 'black'
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ship.move();
-    // ship.velo.x = 0;
-    // ship.velo.y = 0;
-    // ship.rotate = 0
     for (let i = torpedos.length -1; i >= 0; i--) {
         const torpedo = torpedos[i];
-        torpedo .move();
+        torpedo.move();
 
         if (
             torpedo.posit.x + torpedo.size < 0 ||
@@ -196,6 +399,89 @@ function play() {
             torpedo.posit.y - torpedo.size > canvas.height
         ) {
             torpedos.splice(i, 1)
+        }
+    }
+    
+    
+    for (let i = junk.length -1; i >= 0; i--) {
+        const sjunk = junk[i];
+        sjunk.move();
+        
+        if (sjunk.type == 2){
+            let vx;
+            let vy;
+            let dy;
+            let dx;
+            if (Math.random() < .005){
+                console.log('New Mine')
+                if (sjunk.velo.x < 0) {
+                    vx = Math.random() * 1;
+                    dx = 1;
+                } else {
+                    vx = Math.random() * -1;
+                    dx = -1;
+                }
+                if (sjunk.velo.y < 0) {
+                    vy = Math.random() * 1;
+                    dy = 1;
+                } else {
+                    vy = Math.random() * -1;
+                    dy = -1;
+                }
+                mines.push (
+                    new Mine({
+                        posit: {
+                            x: sjunk.posit.x + (sjunk.radius * dx),
+                            y: sjunk.posit.y + (sjunk.radius * dy)
+                        },
+                        velo: {
+                            x: vx,
+                            y: vy
+                        }
+                    
+
+                    })
+                )
+            }
+        }
+
+        // posit: {
+        //     x: ship.posit.x + (Math.cos(ship.rotate)) * 30,
+        //     y: ship.posit.y + (Math.sin(ship.rotate)) * 30,
+
+        if (
+            sjunk.posit.x + sjunk.radius < 0 ||
+            sjunk.posit.x - sjunk.radius > canvas.width ||
+            sjunk.posit.y + sjunk.radius < 0 ||
+            sjunk.posit.y - sjunk.radius > canvas.height
+        ) {
+            junk.splice(i, 1)
+            
+        }
+    }
+    for (let i = mines.length -1; i >= 0; i--) {
+        const smine = mines[i];
+        smine.move();
+    }
+
+    for (let i = qortal.length -1; i >= 0; i--) {
+        const qortal1 = qortal[i];
+        qortal1.move();
+
+        
+    }
+
+    for (let i = mines.length -1; i >= 0; i--) {
+        const smine = mines[i];
+        smine.move();
+
+        if (
+            smine.posit.x + smine.radius < 0 ||
+            smine.posit.x - smine.radius > canvas.width ||
+            smine.posit.y + smine.radius < 0 ||
+            smine.posit.y - smine.radius > canvas.height
+        ) {
+            mines.splice(i, 1)
         }
     }
     
